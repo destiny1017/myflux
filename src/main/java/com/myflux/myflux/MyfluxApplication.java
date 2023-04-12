4537b33f9cb7b8822fa64913bc0ed32672c55cd4
KDH_Notebook <destiny1017@nate.com>
2023-04-12 22:07:58 +0900

    Initial commit

---
+++ src/main/java/com/myflux/myflux/MyfluxApplication.java
@@ -0,0 +1,13 @@
+package com.myflux.myflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfluxApplication.class, args);
+	}
+
+}
